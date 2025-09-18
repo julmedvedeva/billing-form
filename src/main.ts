f128929d0ff25f64a8955c9425feae47b5a05666
Юлия Медведева <60058448+julmedvedeva@users.noreply.github.com>
2025-09-18 17:11:01 +0300

    удалила дефолтные компоненты

--- src/main.ts
+++ src/main.ts
@@ -2,5 +2,10 @@ import './assets/main.css'
 
 import { createApp } from 'vue'
 import App from './App.vue'
+import router from './router'
+import { createPinia } from 'pinia'
 
-createApp(App).mount('#app')
+const app = createApp(App)
+const pinia = createPinia()
+
+app.use(router).use(pinia).mount('#app')
