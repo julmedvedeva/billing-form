dc95813e320e0b3f3dc9c0d9d2771771c3e08cae
Юлия Медведева <60058448+julmedvedeva@users.noreply.github.com>
2025-09-18 16:52:16 +0300

    test commit

---
+++ src/components/NavigationComponent.vue
@@ -0,0 +1,10 @@
+<template>
+  <nav class="bg-red-100">
+    <RouterLink to="/"> Home </RouterLink>
+    <RouterLink to="/chamber-of-secrets"> Chamber of Secrets </RouterLink>
+  </nav>
+</template>
+<script setup lang="ts">
+import { RouterLink } from 'vue-router'
+</script>
+<style scoped></style>
---
+++ test.js
@@ -0,0 +1,2 @@
+const unused = 'test'
+console.log('hello')
